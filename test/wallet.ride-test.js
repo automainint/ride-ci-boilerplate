5ee8c183dcd3fd47988f1d266918ce694f18d0ea
Mitya Selivanov <0x7fffff@guattari.ru>
2022-07-18 13:32:55 +0400

    code format

--- test/wallet.ride-test.js
+++ test/wallet.ride-test.js
@@ -1,65 +1,64 @@
 const wvs = 10 ** 8;
 
 describe('wallet test suite', async function () {
+  this.timeout(100000);
 
-    this.timeout(100000);
+  before(async function () {
+    await setupAccounts(
+      { foofoofoofoofoofoofoofoofoofoofoo: 10 * wvs,
+        barbarbarbarbarbarbarbarbarbar: 2 * wvs,
+        wallet: 0.05 * wvs });
+    const script = compile(file('wallet.ride'));
+    const ssTx = setScript(
+      { script: script },
+      accounts.wallet);
+    await broadcast(ssTx);
+    await waitForTx(ssTx.id)
+    console.log('Script has been set')
+  });
 
-    before(async function () {
-        await setupAccounts(
-            {foofoofoofoofoofoofoofoofoofoofoo: 10 * wvs,
-                 barbarbarbarbarbarbarbarbarbar: 2 * wvs,
-                  wallet: 0.05 * wvs});
-        const script = compile(file('wallet.ride'));
-        const ssTx = setScript({script}, accounts.wallet);
-        await broadcast(ssTx);
-        await waitForTx(ssTx.id)
-        console.log('Script has been set')
-    });
-    
-    it('Can deposit', async function () {
-
-        const iTxFoo = invokeScript({
-            dApp: address(accounts.wallet),
-            call: {function: "deposit"},
-            payment: [{assetId: null, amount: 0.9 * wvs}]
-        }, accounts.foofoofoofoofoofoofoofoofoofoofoo);
-
-
-        const iTxBar = invokeScript({
-            dApp: address(accounts.wallet),
-            call: {function: "deposit"},
-            payment: [{assetId: null, amount: 1.9 * wvs}]
-        }, accounts.barbarbarbarbarbarbarbarbarbar)
+  it('Can deposit', async function () {
+    const iTxFoo = invokeScript(
+      { dApp:     address(accounts.wallet),
+        call:     { function: 'deposit' },
+        payment:  [ { assetId: null, amount: 0.9 * wvs } ] },
+      accounts.foofoofoofoofoofoofoofoofoofoofoo);
 
 
-        await broadcast(iTxFoo);
-        await broadcast(iTxBar);
-        await waitForTx(iTxFoo.id);
-        await waitForTx(iTxBar.id);
-    })
+    const iTxBar = invokeScript(
+      { dApp:     address(accounts.wallet),
+        call:     { function: 'deposit' },
+        payment:  [ { assetId: null, amount: 1.9 * wvs } ] },
+      accounts.barbarbarbarbarbarbarbarbarbar);
 
-    it('Cannot withdraw more than was deposited', async function () {
-        const iTxFoo = invokeScript({
-            dApp: address(accounts.wallet),
-            call: {
-                function: "withdraw",
-                args: [{type:'integer', value: 2 * wvs}]
-            },
 
-        }, accounts.foofoofoofoofoofoofoofoofoofoofoo);
+    await broadcast(iTxFoo);
+    await broadcast(iTxBar);
+    await waitForTx(iTxFoo.id);
+    await waitForTx(iTxBar.id);
+  });
 
-        expect(broadcast(iTxFoo)).to.be.rejectedWith("Not enough balance")
-    })
+  it('Cannot withdraw more than was deposited', async function () {
+    const iTxFoo = invokeScript(
+      { dApp: address(accounts.wallet),
+        call: {
+          function: 'withdraw',
+          args:     [ { type: 'integer', value: 2 * wvs } ]
+        } },
+      accounts.foofoofoofoofoofoofoofoofoofoofoo);
 
-    it('Can withdraw', async function () {
-        const iTxFoo = invokeScript({
-            dApp: address(accounts.wallet),
-            call: {
-                function: "withdraw",
-                args: [{ type: 'integer', value: 0.9 * wvs }]
-            },
+    expect(broadcast(iTxFoo)).to.be.rejectedWith('Not enough balance')
+  });
 
-        }, accounts.foofoofoofoofoofoofoofoofoofoofoo);
-        await broadcast(iTxFoo)
-    })
-})
+  it('Can withdraw', async function () {
+    const iTxFoo = invokeScript(
+      { dApp: address(accounts.wallet),
+        call: {
+          function: 'withdraw',
+          args:     [ { type: 'integer', value: 0.9 * wvs } ]
+        } },
+      accounts.foofoofoofoofoofoofoofoofoofoofoo);
+
+    await broadcast(iTxFoo)
+  });
+});
